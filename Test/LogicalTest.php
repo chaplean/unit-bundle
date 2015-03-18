d4dc647dc30b19c2795e2b08a883a4abd4dfabc1
Matthias Devlamynck <matthias@chaplean.com>
2015-03-18 17:36:14 +0100

    Add LogicalTest to integrate liip's WebTestCase

---
+++ Test/LogicalTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * SeleniumTest.
+ *
+ * @author    Matthias - Chaplean <matthias@chaplean.com>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.com)
+ * @since     0.1.0
+ */
+
+namespace Chaplean\Bundle\UnitBundle\Test;
+
+use Liip\FunctionalTestBundle\Test\WebTestCase;
+
+class LogicalTest extends WebTestCase
+{
+}
