1f544ede91cdfde9157399773457848ed829dc94
Benoit SCHLUPP <benoit@chaplean.com>
2015-03-25 17:01:40 +0100

    change selenium browser to chrome

--- Tests/FunctionalTest.php
+++ Tests/FunctionalTest.php
@@ -29,7 +29,7 @@ class FunctionalTest extends \PHPUnit_Extensions_Selenium2TestCase
 
         $this->baseUrl = $container->getParameter('base_url_selenium');
 
-        $this->setBrowser('firefox');
+        $this->setBrowser('chrome');
         $this->setBrowserUrl($this->baseUrl);
     }
 }
