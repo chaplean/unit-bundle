c0ad5468630bffa50c4a25f0b1ea1e7dc134c16d
Matthias Devlamynck <matthias@chaplean.com>
2015-05-05 16:45:30 +0200

    Add custom kernel

---
+++ BehatKernel.php
@@ -0,0 +1,29 @@
+<?php
+
+include 'app/AppKernel.php';
+
+/**
+ * Class BehatKernel.
+ *
+ * @author    Valentin - Chaplean <valentin@chaplean.com>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.com)
+ * @since     1.1.1
+ */
+class BehatKernel extends AppKernel
+{
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return $this->rootDir.'/../var/logs/'.$this->environment;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return $this->rootDir.'/../var/cache/'.$this->environment;
+    }
+}
