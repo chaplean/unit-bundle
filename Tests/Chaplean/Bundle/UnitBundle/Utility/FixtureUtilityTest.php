2fad330ffed7b833167619b6b3b3aab8715895fb
Valentin Bontemps <valentin@chaplean.com>
2016-05-14 18:50:22 +0200

    try fix that

--- Tests/Chaplean/Bundle/UnitBundle/Utility/FixtureUtilityTest.php
+++ Tests/Chaplean/Bundle/UnitBundle/Utility/FixtureUtilityTest.php
@@ -34,8 +34,8 @@ class FixtureUtilityTest extends LogicalTest
     public function testLoadDefaultFixturesWithNamespace()
     {
         $this->assertEquals(array(
-            'Chaplean\Bundle\UnitBundle\DataFixtures\Liip\DefaultData\LoadClientData',
             'Chaplean\Bundle\UnitBundle\DataFixtures\Liip\DefaultData\LoadStatusData',
+            'Chaplean\Bundle\UnitBundle\DataFixtures\Liip\DefaultData\LoadClientData',
         ), FixtureUtility::loadDefaultFixtures($this->getNamespace()));
     }
 
