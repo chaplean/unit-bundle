995d06082443640922a17d77bfd3c70e6f8f55a6
Tom - Chaplean <tom@chaplean.com>
2016-07-10 11:34:32 +0200

    Bash script to wait until mysqld is launched

--- bin/services-waiting.sh
+++ bin/services-waiting.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+# Argument 1 : mysql service
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  exit 2
+elif [ $# -gt 1 ]; then
+  echo 1>&2 "$0: too many arguments"
+  exit 2
+fi
+
 function test_mysql {
-  mysqladmin -h "mysql" -uroot -proot ping
+  mysqladmin -h $1 -uroot -proot ping
 }
 
 count=0
 # Chain tests together by using &&
-until ( test_mysql )
+until ( test_mysql $1 )
 do
   ((count++))
   if [ ${count} -gt 100 ]
--- phpunit.sh
+++ phpunit.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
-docker exec unit_bundle phpunit --configuration ./phpunit_default.xml --coverage-php build/logs/clover-default.xml
-docker exec unit_bundle phpunit --configuration ./phpunit_sqlite.xml --coverage-php build/logs/clover-sqlite.xml
+# Argument 1 : mysql service
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: not enough arguments"
+  exit 2
+elif [ $# -gt 1 ]; then
+  echo 1>&2 "$0: too many arguments"
+  exit 2
+fi
+
+./bin/services-waiting.sh $1
+
+phpunit --configuration ./phpunit_default.xml --coverage-php build/logs/clover-default.xml
+phpunit --configuration ./phpunit_sqlite.xml --coverage-php build/logs/clover-sqlite.xml
