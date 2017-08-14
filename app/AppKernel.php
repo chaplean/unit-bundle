f5a463ae3ac3b7f59724a8a8fbb79bd4edd46717
Tom - Chaplean <tom@chaplean.coop>
2017-08-14 11:39:43 +0200

    Removing useless configs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,10 +22,8 @@ class AppKernel extends Kernel
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Chaplean\Bundle\UnitBundle\ChapleanUnitBundle(),
             new Liip\FunctionalTestBundle\LiipFunctionalTestBundle(),
             new FOS\RestBundle\FOSRestBundle(),
