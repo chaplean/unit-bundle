633c9b4c1e55e28acc30cdea8e831fa1c21cdceb
Valentin <valentin@chaplean.coop>
2017-12-06 15:52:32 +0100

    #24484 clean X.Y.Z

--- Tests/Chaplean/Bundle/UnitBundle/Utility/ServiceDependentSharedServiceTest.php
+++ Tests/Chaplean/Bundle/UnitBundle/Utility/ServiceDependentSharedServiceTest.php
@@ -11,7 +11,7 @@ use Liip\FunctionalTestBundle\Test\WebTestCase;
  * @package   Tests\Chaplean\Bundle\UnitBundle\Utility
  * @author    Valentin - Chaplean <valentin@chaplean.coop>
  * @copyright 2014 - 2016 Chaplean (http://www.chaplean.coop)
- * @since     X.Y.Z
+ * @since     5.0.0
  */
 class ServiceDependentSharedServiceTest extends WebTestCase
 {
